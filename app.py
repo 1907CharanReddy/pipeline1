b18a9872f5df3017f02d00baaf6f26594648550e
1907CharanReddy <saicharanreddy2007@gmail.com>
2026-05-23 11:51:49 +0530

    pipeline1

---
+++ app.py
@@ -0,0 +1,21 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Division by zero error!"
+    return a / b
+
+if __name__ == "__main__":
+    x, y = 10, 5
+    print(f"Addition: {add(x, y)}")
+    print(f"Subtraction: {subtract(x, y)}")
+    print(f"Multiplication: {multiply(x, y)}")
+    print(f"Division: {divide(x, y)}")
+
